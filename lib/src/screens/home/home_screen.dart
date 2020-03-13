1253490f9d65c869375b4e96890c81106c0148da
Antonio Filho <antonio.ads0394@gmail.com>
2020-03-13 09:43:57 -0300

    dispose reactions

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -1,20 +1,38 @@
 import 'package:firebase_mobx/src/screens/login/login_screen.dart';
+import 'package:firebase_mobx/src/stores/login/firebase_login_store.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
 
-import 'package:firebase_mobx/src/stores/login/firebase_login_store.dart';
-
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const routeName = '/home';
 
   @override
-  Widget build(BuildContext context) {
-    var _loginStore = Provider.of<FirebaseLoginStore>(context, listen: false);
-    when(
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  FirebaseLoginStore _loginStore;
+  ReactionDisposer _loginListenDisposer;
+
+  @override
+  void initState() {
+    _loginStore = Provider.of<FirebaseLoginStore>(context, listen: false);
+    _loginListenDisposer = when(
       (_) => !_loginStore.signedIn,
       () => Navigator.pushReplacementNamed(context, LoginScreen.routeName),
     );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _loginListenDisposer();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
--- lib/src/screens/login/login_screen.dart
+++ lib/src/screens/login/login_screen.dart
@@ -1,20 +1,38 @@
+import 'package:firebase_mobx/src/screens/home/home_screen.dart';
+import 'package:firebase_mobx/src/stores/login/firebase_login_store.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
 
-import 'package:firebase_mobx/src/screens/home/home_screen.dart';
-import 'package:firebase_mobx/src/stores/login/firebase_login_store.dart';
-
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   static const routeName = '/login';
 
   @override
-  Widget build(BuildContext context) {
-    var _loginStore = Provider.of<FirebaseLoginStore>(context, listen: false);
-    when(
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  FirebaseLoginStore _loginStore;
+  ReactionDisposer _loginListenDisposer;
+
+  @override
+  void initState() {
+    _loginStore = Provider.of<FirebaseLoginStore>(context, listen: false);
+    _loginListenDisposer = when(
       (_) => _loginStore.signedIn,
       () => Navigator.pushReplacementNamed(context, HomeScreen.routeName),
     );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _loginListenDisposer();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
